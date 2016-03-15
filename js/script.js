bd86b6da512b0ff29a9ea221c940273d92b8f77f
AlinaLanina <123Ga456>
2016-03-15 15:30:27 +0300

    15/03

--- js/script.js
+++ js/script.js
@@ -1,58 +1,35 @@
 
+// $(document).ready(function() {
+//   $(".fancybox").fancybox({
+//     openEffect  : 'fade',
+//     closeEffect : 'none'
+//   });
+// });
 
-    $(document).ready(function() {
+// $(document).ready(function() {
+// $("a.fancybox").fancybox({
+//   overlayShow : true,
+// 'transitionIn' : 'elastic',
+// 'transitionOut' : 'elastic',
+// 'speedIn' : 600,
+// 'speedOut' : 200,
+//  //затемнение основной страницы (фон) true/false
+// 'overlayOpacity' : 0.8, //прозрачность фона
+// 'overlayColor' : '#777', //цвет фона
+// 'titleShow' : false, //вывод надписей под изображениями
+// 'width' : 560, //ширина окна
+// 'height' : 340 //высота окна
+
+
+// });
+// });
+
+$(document).ready(function() {
 $("a.fancybox").fancybox({
 'transitionIn' : 'elastic',
 'transitionOut' : 'elastic',
 'speedIn' : 600,
 'speedOut' : 200,
-'overlayShow' : false
+'overlayShow' : true
 });
-
-(function ($, F) {
-    F.transitions.resizeIn = function() {
-        var previous = F.previous,
-            current  = F.current,
-            startPos = previous.wrap.stop(true).position(),
-            endPos   = $.extend({opacity : 1}, current.pos);
-
-        startPos.width  = previous.wrap.width();
-        startPos.height = previous.wrap.height();
-
-        previous.wrap.stop(true).trigger('onReset').remove();
-
-        delete endPos.position;
-
-        current.inner.hide();
-
-        current.wrap.css(startPos).animate(endPos, {
-            duration : current.nextSpeed,
-            easing   : current.nextEasing,
-            step     : F.transitions.step,
-            complete : function() {
-                F._afterZoomIn();
-
-                current.inner.fadeIn("fast");
-            }
-        });
-    };
-
-}(jQuery, jQuery.fancybox));
-
-$(".fancybox")
-    .attr('rel', 'gallery')
-    .fancybox({
-        nextMethod : 'resizeIn',
-        nextSpeed  : 250,
-
-        prevMethod : false,
-
-        helpers : {
-            title : {
-                type : 'inside'
-            }
-        }
-    });
-
 });
-
