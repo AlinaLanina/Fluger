e3df13145014465035bf9efac3de188b7c22fd71
AlinaLanina <123Ga456>
2016-03-15 13:20:24 +0300

    15.03

--- js/script.js
+++ js/script.js
@@ -1,5 +1,58 @@
-<script type="text/javascript">
-  $(document).ready(function() {
-    $(".fancybox").fancybox();
-  });
-</script>
+
+
+    $(document).ready(function() {
+$("a.fancybox").fancybox({
+'transitionIn' : 'elastic',
+'transitionOut' : 'elastic',
+'speedIn' : 600,
+'speedOut' : 200,
+'overlayShow' : false
+});
+
+(function ($, F) {
+    F.transitions.resizeIn = function() {
+        var previous = F.previous,
+            current  = F.current,
+            startPos = previous.wrap.stop(true).position(),
+            endPos   = $.extend({opacity : 1}, current.pos);
+
+        startPos.width  = previous.wrap.width();
+        startPos.height = previous.wrap.height();
+
+        previous.wrap.stop(true).trigger('onReset').remove();
+
+        delete endPos.position;
+
+        current.inner.hide();
+
+        current.wrap.css(startPos).animate(endPos, {
+            duration : current.nextSpeed,
+            easing   : current.nextEasing,
+            step     : F.transitions.step,
+            complete : function() {
+                F._afterZoomIn();
+
+                current.inner.fadeIn("fast");
+            }
+        });
+    };
+
+}(jQuery, jQuery.fancybox));
+
+$(".fancybox")
+    .attr('rel', 'gallery')
+    .fancybox({
+        nextMethod : 'resizeIn',
+        nextSpeed  : 250,
+
+        prevMethod : false,
+
+        helpers : {
+            title : {
+                type : 'inside'
+            }
+        }
+    });
+
+});
+
