402516a04b979610e1689a854aa6558d9716056f
AlinaLanina <123Ga456>
2016-03-16 13:42:05 +0300

    16.03

--- js/script.js
+++ js/script.js
@@ -1,35 +1,14 @@
 
-// $(document).ready(function() {
-//   $(".fancybox").fancybox({
-//     openEffect  : 'fade',
-//     closeEffect : 'none'
-//   });
-// });
-
-// $(document).ready(function() {
-// $("a.fancybox").fancybox({
-//   overlayShow : true,
-// 'transitionIn' : 'elastic',
-// 'transitionOut' : 'elastic',
-// 'speedIn' : 600,
-// 'speedOut' : 200,
-//  //затемнение основной страницы (фон) true/false
-// 'overlayOpacity' : 0.8, //прозрачность фона
-// 'overlayColor' : '#777', //цвет фона
-// 'titleShow' : false, //вывод надписей под изображениями
-// 'width' : 560, //ширина окна
-// 'height' : 340 //высота окна
-
-
-// });
-// });
-
 $(document).ready(function() {
 $("a.fancybox").fancybox({
-'transitionIn' : 'elastic',
-'transitionOut' : 'elastic',
-'speedIn' : 600,
-'speedOut' : 200,
-'overlayShow' : true
+
+   helpers : {
+        overlay : {
+            css : {
+                'background' : 'rgba(58, 42, 45, 0.95)'
+            }
+        }
+    }
+
 });
 });
